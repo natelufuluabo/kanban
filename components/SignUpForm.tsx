6fe76bdacbf6aa28618de3695f98e4c2abace49b
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-07-31 23:13:01 -0400

    installed email and password validators package to check email and password entered in the signup form

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -1,34 +1,71 @@
-import { Dispatch, SetStateAction } from "react";import styles from "./SignUpForm.module.scss";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
+import styles from "./SignUpForm.module.scss";
+import { User } from "@/classes/User";
 
 interface propsType {
   setSignUpShowing: Dispatch<SetStateAction<boolean>>
 }
 
 export const SignUpForm = ({ setSignUpShowing }: propsType) => {
-    return (
-        <div className={styles.signUpFormContainer}>
-            <form className={styles.signUpForm}>
-              <div className={styles.topSection}>
-                <div className={styles.textContainer}>
-                  <h2>Sign Up</h2>
-                  <p>It&apos;s quick and easy.</p>
-                </div>
-                <button onClick={() => setSignUpShowing(false)} className={styles.closeFormButton}>X</button>
-              </div>
-              <div className={styles.inputContainer}>
-                <input placeholder='First Name' />
-              </div>
-              <div className={styles.inputContainer}>
-                <input placeholder='Last Name' />
-              </div>
-              <div className={styles.inputContainer}>
-                <input placeholder='Email' />
-              </div>
-              <div className={styles.inputContainer}>
-                <input placeholder='Password' />
-              </div>
-              <button onClick={() => setSignUpShowing(false)} className={styles.signUpButton}>Sign Up</button>
-            </form>
+  const [formDatas, setFormDatas] = useState<User>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    const newUser = new User(formDatas.firstName, formDatas.lastName, formDatas.email, formDatas.password);
+    console.log(newUser);
+  };
+  return (
+    <div className={styles.signUpFormContainer}>
+      <form className={styles.signUpForm} onSubmit={handleSubmit}>
+        <div className={styles.topSection}>
+          <div className={styles.textContainer}>
+            <h2>Sign Up</h2>
+            <p>It&apos;s quick and easy.</p>
+          </div>
+          <button onClick={() => setSignUpShowing(false)} className={styles.closeFormButton}>X</button>
         </div>
-    )
+        <div className={styles.inputContainer}>
+          <input 
+            type="text"
+            placeholder='First Name'
+            name="firstName"
+            value={formDatas.firstName}
+            onChange={(evt) => setFormDatas({ ...formDatas, [evt.target.name]: evt.target.value})} 
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <input 
+            type="text"
+            placeholder='Last Name'
+            name="lastName"
+            value={formDatas.lastName}
+            onChange={(evt) => setFormDatas({ ...formDatas, [evt.target.name]: evt.target.value})}  
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <input 
+            type="email"
+            placeholder='Email'
+            name="email"
+            value={formDatas.email}
+            onChange={(evt) => setFormDatas({ ...formDatas, [evt.target.name]: evt.target.value})} 
+          />
+        </div>
+        <div className={styles.inputContainer}>
+          <input 
+            type="password"
+            placeholder='Password' 
+            name="password"
+            value={formDatas.password}
+            onChange={(evt) => setFormDatas({ ...formDatas, [evt.target.name]: evt.target.value})}  
+          />
+        </div>
+        <button type="submit" className={styles.signUpButton}>Sign Up</button>
+      </form>
+    </div>
+  )
 };

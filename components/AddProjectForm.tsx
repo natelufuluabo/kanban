71801401c9162edfc46f07f1d264000332a4efa3
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-09-06 21:45:58 -0400

    successfully implemented the ability to add new project. process is still slow to complete. Gotta investigate what is making it so slow

--- components/AddProjectForm.tsx
+++ components/AddProjectForm.tsx
@@ -1,26 +1,55 @@
-import { Dispatch, SetStateAction, useState } from "react";
+"use client";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
 import styles from "./AddProjectForm.module.scss";
 import { DualRingComponent } from "./DualRing";
+import { addProject } from "@/firebase/projectsCollection";
+import { userDataAtom } from "@/app/recoilContextProvider";
+import { useRecoilValue } from "recoil";
 
 interface propsType {
     setNewProjectFormState: Dispatch<SetStateAction<boolean>>
 }
 
 export const AddProjectForm = ({ setNewProjectFormState }: propsType) => {
+    const userData = useRecoilValue(userDataAtom);
+    console.log(userData?.id);
     const [waiting, setWaiting] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [title, setTitle] = useState("");
+    const handleSubmit = async (evt: FormEvent<HTMLFormElement>) => {
+        evt.preventDefault();
+        setWaiting(true);
+        if (title.length === 0) {
+            setErrorMessage("Title required");
+            setWaiting(false);
+            return;
+        }
+        await addProject(title, userData?.id);
+        setWaiting(false);
+        setNewProjectFormState(false);
+    };
+    const handleCancel = () => {
+        setTitle("");
+        setNewProjectFormState(false);
+    };
     return (
         <div className={styles.formContainer}>
-            <form className={styles.addProjectForm}>
+            <form className={styles.addProjectForm} onSubmit={handleSubmit}>
                 <div className={styles.inputFormContainer}>
                     <input 
                         type='text'
                         placeholder='Project title'
                         name="title"
+                        value={title}
+                        onChange={(evt) => setTitle(evt.target.value)}
                     />
                 </div>   
+                <div className={styles.errorContainer}>
+                    <p className={styles.errorMessage}>{errorMessage}</p>
+                </div>
                 <div className={styles.buttonsContainer}>
                     <button className={styles.addProjectButton}>Submit { waiting && <DualRingComponent /> }</button>
-                    <button onClick={() => setNewProjectFormState(false)} className={styles.cancelButton}>Cancel</button>
+                    <button onClick={handleCancel} className={styles.cancelButton}>Cancel</button>
                 </div>   
             </form>
         </div>
